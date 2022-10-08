1d82ec9a50bf60f42fb30d2a85b922157934f2db
Rettend <hegyi.aron101@gmail.com>
2022-10-08 18:05:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,14 +109,6 @@ async def run(cmd):
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
 
-    stdout, stderr = await proc.communicate()
-
-    print(f"[{cmd!r} exited with {proc.returncode}]")
-    if stdout:
-        print(f"[stdout]\n{stdout.decode()}")
-    if stderr:
-        print(f"[stderr]\n{stderr.decode()}")
-
 
 async def main():
     await asyncio.gather(
